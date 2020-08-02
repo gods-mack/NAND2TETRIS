de586794c5910fa97dfbfb775d5b24a2d20b0970
gods-mack <manish-1004@outlook.com>
2020-08-02 19:22:01 +0530

    Hack Assembler

---
+++ 06/assembler/pass1.cpp
@@ -0,0 +1,21 @@
+
+
+
+#include<bits/stdc++.h>
+#include "pass1.h"
+using namespace std;
+
+void label_parser(ifstream & fin){
+
+	string line = "";
+	while(fin) {
+		getline(fin,line);
+		cout<<line<<endl;
+	}
+
+	
+}
+/*
+void hello() {
+	cout<<"hey Manish";
+}*/
---
+++ 06/assembler/pass1.h
@@ -0,0 +1,11 @@
+
+#ifndef _PASS1_H
+#define _PASS1_H
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//void hello();
+void label_parser(ifstream&);
+
+#endif
